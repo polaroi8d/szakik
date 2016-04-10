a0ec81b60d6a8527c4d6695f67ce15d22ef02350
varga04 <varga.danielr@gmail.com>
2016-04-10 12:43:35 +0200

    UTF-8

--- connect.inc.php
+++ connect.inc.php
@@ -4,7 +4,7 @@ $oracle_user = 'system';
 $oracle_pass = 'admin';
 
 
-$conn = oci_connect($oracle_user, $oracle_pass, $oracle_host);
+$conn = oci_connect($oracle_user, $oracle_pass, $oracle_host,'AL32UTF8');
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
