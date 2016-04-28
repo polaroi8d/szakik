ed6b51f7e4498dc7a33f6f535957fcc14185a8e5
varga04 <varga.danielr@gmail.com>
2016-04-28 19:08:05 +0200

    fejléc update

--- fejlec.php
+++ fejlec.php
@@ -5,14 +5,23 @@
         <li class="active"><a href="index.php">Kezdőoldal</a></li>
         <li class="last"><a href="browse.php">Böngészés</a></li>
         <li class="last"><a href="top.php">Legek</a></li>
-		<li class="last"><a href="panasz.php">Panasz</a></li>
-		 <li class="last"><a href="ellenorzo.php">Ellenőrzés</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
 	<?php
 	if (isset($_SESSION['user'])){
 		//echo $_SESSION['user'] . '!';
 		echo  '<li><a href="logout.php"><span class="glyphicon glyphicon-remove"></span> Kilépés</a></li>';
+		$user = $_SESSION['user'];
+		$query = "SELECT COUNT(*) AS NUMBER_OF_ROWS FROM FELHASZNALO WHERE FELHASZNALONEV = '" . $user . "'"  ;
+		$stid = oci_parse($conn, $query);
+		oci_define_by_name($stid, 'NUMBER_OF_ROWS', $number_of_rows);
+		oci_execute($stid);
+		oci_fetch($stid);
+		if ($number_of_rows == 0)
+	{
+		$a=1;
+	} else { $a=0; echo '
+		<li class="last"><a href="panasz.php">Panasz</a></li>';}
 	}else{
 		echo  '<li class="dropdown">';
         echo  '<a href="#" data-toggle="dropdown" class="dropdown-toggle">';
